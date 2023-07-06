c19ae1e195605b0ed53c3972823d0dc3b16621fe
capriciousbased <mdanisulhoquekhan@gmail.com>
2023-07-06 21:53:50 +0200

    reminder box added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import './App.css'
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import DateTimePicker from "react-datetime-picker"
 
-import './App.css';
 
 function App() {
+
+  const [reminderMsg, setReminderMsg] = useState("")
+  const [remindAt, setRemindAt] = useState()
+  const addReminder = () => {
+
+  }
   return (
     <div className="App">
-    <h1>Reminder App</h1>
+      <div className="homepage">
+        <div className="homepage_header">
+          <h1>Remind Me</h1>
+          <input type="text" placeholder='Reminder node here...' value={reminderMsg} onChange={e => setReminderMsg(e.target.value)} />
+          <DateTimePicker
+            value={remindAt}
+            onChange={setRemindAt}
+            minDate={new Date()}
+            minutePlaceholder="mm"
+            hourPlaceholder="hh"
+            dayPlaceholder="DD"
+            monthPlaceholder="MM"
+            yearPlaceholder="YYYY"
+          />
+          <div className="button" onClick={addReminder}>Add Reminder</div>
+        </div>
+      </div>
     </div>
   );
 }
